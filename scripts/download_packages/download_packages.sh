b136ffbf9a4ebec10c1b7ef1e2aa293bc8c050c7
ocrossi <zhdoctor971@gmail.com>
2024-11-19 06:23:20 -0500

    fix expat

--- scripts/download_packages/download_packages.sh
+++ scripts/download_packages/download_packages.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # stop on any error
-set -euo pipefail
 
-if [ -z ${LFS}]; then
+if [ -z ${LFS} ]; then
 		echo "LFS env var is not defined, source /root/.zprofile"
 		return
 	else
@@ -12,7 +11,7 @@ if [ -z ${LFS}]; then
 fi
 
 # create source dir to contain packages to install for x compile toolchain
-mkdir -v $LFS/sources
+mkdir -pv $LFS/sources
 chmod -v a+wt $LFS/sources
 
 # add packages & corrections to sources dur
