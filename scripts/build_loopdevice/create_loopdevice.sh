ca3176ed37737a424bae969c12b08df7cdc44f11
Octave Rossi <ocrossi@z3r4p5.42lyon.fr>
2024-11-22 17:40:44 +0100

    use qcow 2 for all now

--- scripts/build_loopdevice/create_loopdevice.sh
+++ scripts/build_loopdevice/create_loopdevice.sh
@@ -4,7 +4,8 @@
 set -euo pipefail
 
 config_file="/root/.zprofile"
-file="/disk.img"
+raw_file="./disk.raw"
+qcow2_file="/lfs.qcow2"
 partition_blueprint="partitions.txt"
 lfs_dir="/mnt/lfs"
 output_file="out.txt"
@@ -16,47 +17,35 @@ function display_command() {
 	sleep 1
 }
 
-#display_command 	"create file of size 20G filled with 0s " \
-#			'dd if=/dev/zero of=$file bs=1M count=20480 &>/dev/null'
-
+free_networkdevice="/dev/nbd0"
 display_command 	"create file of size 20G filled with 0s " \
-			'qemu-img create $file 20G'
-display_command 	"find 1st free loopdevice & store it into a variable " \
-			'free_loopdevice=`losetup -f`'
-display_command 	"attach $file to loopdevice $free_loopdevice  " \
-			'losetup $free_loopdevice $file'
-display_command 	"confirm creation of loopdevice  " \
-			'losetup $free_loopdevice &>/dev/null'
-display_command		"partition the loopdevice file using $partition_blueprint  " \
-			'cat $partition_blueprint | sfdisk $file &>$output_file'
-display_command 	"unattach loop device" \
-			'losetup -d $free_loopdevice'
-display_command 	"reattach loop device while scanning for partitions" \
-			'losetup -P $free_loopdevice $file'
-
-home_part=$free_loopdevice"p2"
-swap_part=$free_loopdevice"p3"
-
+			'qemu-img create $raw_file 20G'
+display_command		"partition the network device block file using $partition_blueprint  " \
+			'cat $partition_blueprint | sfdisk $raw_file &>$output_file'
+boot_part=$free_networkdevice"p1"
+home_part=$free_networkdevice"p2"
+swap_part=$free_networkdevice"p3"
+display_command		"activate kernel module NBDd (NetworkBlockDevice)" \
+			'modprobe nbd max_part=8'
+display_command		"add $lfs_dir to zprofile" \
+			'echo "export LFS=$lfs_dir" >> $config_file'
+display_command		"convert disk image from raw to qcow2" \
+			'qemu-img convert -f raw -O qcow2 $raw_file $qcow2_file'
+display_command		"connect NetworkBlockDevice to disk image" \
+			'qemu-nbd -c $free_networkdevice $qcow2_file'
+display_command		"Change partition table with new NetworkBlockDevice entry" \
+			'partprobe $free_networkdevice'
 display_command		"make filesystem ext4 on home partition" \
 			'mkfs -v -t ext4 $home_part &>$output_file'
 display_command		"make swap partition on last partition " \
 			'mkswap $swap_part &>$output_file'
-display_command		"add $lfs_dir to zprofile" \
-			'echo "export LFS=$lfs_dir" >> $config_file'
 display_command		"create lfs dir" \
 			'mkdir -pv $lfs_dir'
 display_command 	"mount home part from loopfile to $lfs_dir" \
 			'mount $home_part $lfs_dir'
-
-echo toto > /mnt/lfs/tesst
-
+display_command		"create mount dir" \
+			'mkdir -pv /mnt/lfs/boot'
+display_command 	"mount boot part from loopfile to $lfs_dir/boot" \
+			'mount $boot_part $lfs_dir/boot'
 
 # zprofile for reboot
-display_command 	"adding loopdevice as environment to .zprofile file  " \
-			'echo "export LOOPDEVICE=$free_loopdevice" >> $config_file'
-display_command 	"add previous commands to $config_file" \
-			'echo "losetup -P $free_loopdevice $file" >> $config_file'
-display_command		"add mount lfs dir to zprofile" \
-			'echo "mount $home_part $lfs_dir" >> $config_file'
-
-
